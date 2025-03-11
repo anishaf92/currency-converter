6dad4687057047ff4260234d7a00ae353cb43c7b
anishaf92 <anishaf92@gmail.com>
2025-03-11 19:33:06 +0530

    env variables added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
+const apiUrl = import.meta.env.VITE_Access_key;
 function App() {
   const [goldRate, setGoldRate] = useState("")
   const [amount, setAmount] = useState(0)
   const [livePrice, setLivePrice] = useState(null)
   const [totalAmount, setTotalAmount] = useState(null)
   function getRate(){
-    axios.get(`https://api.metalpriceapi.com/v1/convert?api_key=5d4665a0dd6fdffc6a9ee772ba9710b1&from=USD&to=INR&amount=${amount}&date=2025-03-01`)
+    axios.get(`https://api.metalpriceapi.com/v1/convert?api_key=${apiUrl}&from=USD&to=INR&amount=${amount}&date=2025-03-01`)
     .then((response) => {
       console.log(response.data.result)
       setLivePrice(response.data.result)
